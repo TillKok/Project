5691c8958a3ec3805200a3336b5c0e7e23cea83a
TillKok <Cty960904@163.com>
2019-06-30 17:51:36 +0800

    编辑文章-动态渲染编辑页面

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -63,7 +63,11 @@
         <el-table-column prop="pubdate" label="发布时间" width="180"></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" plain>修改</el-button>
+            <el-button
+             size="mini"
+             type="primary"
+             plain
+             @click="$router.push(`/publish/${scope.row.id}`)">修改</el-button>
             <el-button size="mini" type="danger" plain @click="handleDelete(scope.row)">删除</el-button>
           </template>
         </el-table-column>
